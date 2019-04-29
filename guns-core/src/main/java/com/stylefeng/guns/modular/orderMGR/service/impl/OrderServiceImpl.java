f639ecb8414b04ecdde22f08dce7e6c1e0c52917
18580255110 <181908570@qq.com>
2019-04-29 13:30:29 +0800

    4

--- guns-core/src/main/java/com/stylefeng/guns/modular/orderMGR/service/impl/OrderServiceImpl.java
+++ guns-core/src/main/java/com/stylefeng/guns/modular/orderMGR/service/impl/OrderServiceImpl.java
@@ -27,7 +27,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.plugin2.message.Message;
 
 import java.util.*;
 
