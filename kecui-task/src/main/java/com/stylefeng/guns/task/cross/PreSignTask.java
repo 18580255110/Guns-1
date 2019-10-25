e22172a614e292ab2f6932de2d66ed4b58d93689
18580255110 <181908570@qq.com>
2019-10-25 23:58:02 +0800

    20191025 - 3

--- kecui-task/src/main/java/com/stylefeng/guns/task/cross/PreSignTask.java
+++ kecui-task/src/main/java/com/stylefeng/guns/task/cross/PreSignTask.java
@@ -10,6 +10,7 @@ import com.stylefeng.guns.util.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Date;
 import java.util.List;
